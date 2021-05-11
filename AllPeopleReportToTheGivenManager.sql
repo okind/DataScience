5e876c996a0102057eca23d55220650d0f826d2f
okind <okinduser@gmail.com>
2021-05-11 16:39:00 -0700

    All People Report to the Given Manager - Solution - Improve SQL formatting

--- AllPeopleReportToTheGivenManager.sql
+++ AllPeopleReportToTheGivenManager.sql
@@ -1,14 +1,11 @@
-SELECT employee_id
+SELECT Employee_id
 FROM Employees
-WHERE manager_id IN (
-		SELECT employees.employee_id
-		FROM Employees AS employees
-		INNER JOIN Employees AS managers ON employees.manager_id = managers.employee_id
-		/*exclude boss, exclude directly reporting to boss, exclude manager that do not report boss*/
-		WHERE employees.manager_id = 1
-			OR (
-				employees.employee_id <> employees.manager_id
-				AND managers.manager_id = 1
-				)
-		)
-	AND employee_id <> manager_id
+WHERE Manager_id IN
+    (SELECT Employees.Employee_id
+     FROM Employees AS Employees
+     INNER JOIN Employees AS Managers ON Employees.Manager_id = Managers.Employee_id
+     /*exclude boss, exclude directly reporting to boss, exclude manager that do not report boss*/
+     WHERE Employees.Manager_id = 1
+       OR (Employees.Employee_id <> Employees.Manager_id
+           AND Managers.Manager_id = 1) )
+  AND Employee_id <> Manager_id
