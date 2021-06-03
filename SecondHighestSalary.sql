1e95c6e453f0534b892b559b2965fb5af7abe94f
okind <okinduser@gmail.com>
2021-06-02 17:02:44 -0700

    Second highest salary - improved formatting

--- SecondHighestSalary.sql
+++ SecondHighestSalary.sql
@@ -1,11 +1,5 @@
-select (
-    select
-        distinct e.Salary
-    from
-        Employee e
-    order by
-        e.Salary desc
-    offset
-        1 ROWS
-    fetch
-        NEXT 1 ROWS ONLY) as SecondHighestSalary
+SELECT
+  (SELECT DISTINCT E.Salary
+   FROM Employee E
+   ORDER BY E.Salary DESC
+   OFFSET 1 ROWS FETCH NEXT 1 ROWS ONLY) AS Secondhighestsalary
